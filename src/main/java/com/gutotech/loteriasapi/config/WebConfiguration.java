597343fec27117b2e63d841b9a3b69692221841e
guto-alves <gustavo.almeida13@fatec.sp.gov.br>
2021-11-26 17:04:06 -0300

    Disable CORS support

---
+++ src/main/java/com/gutotech/loteriasapi/config/WebConfiguration.java
@@ -0,0 +1,14 @@
+package com.gutotech.loteriasapi.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfiguration implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**").allowedMethods("*");
+    }
+}
