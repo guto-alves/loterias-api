e850dace44fc766ef33045158b85bf2591d20482
Gustavo Alves <gustavo.almeida13@fatec.sp.gov.br>
2021-12-23 01:23:39 -0300

    Create ApiRestControllerTests.java
    Add ApiRestControllerTests

---
+++ src/test/java/com/gutotech/loteriasapi/rest/ApiRestControllerTests.java
@@ -0,0 +1,49 @@
+package com.gutotech.loteriasapi.rest;
+
+import static org.hamcrest.Matchers.hasSize;
+import static org.springframework.test.web.servlet.ResultMatcher.matchAll;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.web.servlet.MockMvc;
+
+@ActiveProfiles("dev")
+@SpringBootTest
+@AutoConfigureMockMvc
+class ApiRestControllerTests {
+
+	@Autowired
+	private MockMvc mockMvc;
+
+	@Test
+	void testGetLotteries() throws Exception {
+		mockMvc.perform(get("/api"))
+			.andDo(print())
+			.andExpect(matchAll(
+				status().isOk(),
+				jsonPath("$", hasSize(9)),
+				jsonPath("$.[0]").value("mega-sena"))
+			);
+	}
+	
+	@Test
+	void testGetResultByIdSuccess() throws Exception {
+		mockMvc.perform(get("/api/mega-sena/1"))
+			.andDo(print())
+			.andExpect(matchAll(
+				status().isOk(),
+				jsonPath("nome").value("Mega-Sena"),
+				jsonPath("concurso").value(1),
+				jsonPath("dezenas", hasSize(6)),
+				jsonPath("timeCoracao").doesNotExist())
+			);
+	}
+
+}
