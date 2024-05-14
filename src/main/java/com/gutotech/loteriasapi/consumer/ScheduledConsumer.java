dec490f8616d7c9e03e9ea6d06621ad39b2dd90a
Gustavo Alves <gustavoalvesb.dealmeida@gmail.com>
2024-05-14 10:39:29 -0300

    Change the check date format to "dd/MM/yyyy HH:mm" and refactor ScheduledConsumer

--- src/main/java/com/gutotech/loteriasapi/consumer/ScheduledConsumer.java
+++ src/main/java/com/gutotech/loteriasapi/consumer/ScheduledConsumer.java
@@ -1,6 +1,8 @@
 package com.gutotech.loteriasapi.consumer;
 
-import java.time.Instant;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.EnableScheduling;
@@ -14,43 +16,51 @@ public class ScheduledConsumer {
     @Autowired
     private LoteriasUpdate loteriasUpdate;
 
-    @Scheduled(cron = "0 0 12 * * MON-SAT", zone = "America/Sao_Paulo")
+    private final String zone = "America/Sao_Paulo";
+    private final DateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy HH:mm");
+
+    private String getCurrentDate() {
+	return dateFormat.format(new Date());
+    }
+
+    @Scheduled(cron = "0 0 12 * * MON-SAT", zone = zone)
     public void checkForNewResults12() {
-	System.out.println("The time is now " + Instant.now());
 	checkForUpdates();
     }
 
-    @Scheduled(cron = "0 0 21 * * MON-SAT", zone = "America/Sao_Paulo")
+    @Scheduled(cron = "0 0 21 * * MON-SAT", zone = zone)
     public void checkForNewResults21() {
-	System.out.println("The time is now " + Instant.now());
 	checkForUpdates();
     }
 
-    @Scheduled(cron = "0 15 21 * * MON-SAT", zone = "America/Sao_Paulo")
+    @Scheduled(cron = "0 15 21 * * MON-SAT", zone = zone)
     public void checkForNewResults2115() {
-	System.out.println("The time is now " + Instant.now());
 	checkForUpdates();
     }
 
-    @Scheduled(cron = "0 0 22 * * MON-SAT", zone = "America/Sao_Paulo")
+    @Scheduled(cron = "0 0 22 * * MON-SAT", zone = zone)
     public void checkForNewResults22() {
-	System.out.println("The time is now " + Instant.now());
 	checkForUpdates();
     }
 
-    @Scheduled(cron = "0 20 0 * * MON-SAT", zone = "America/Sao_Paulo")
+    @Scheduled(cron = "0 10 23 * * MON-SAT", zone = zone)
+    public void checkForNewResults2310() {
+	checkForUpdates();
+    }
+
+    @Scheduled(cron = "0 20 0 * * MON-SAT", zone = zone)
     public void checkForNewResults0020() {
-	System.out.println("The time is now " + Instant.now());
 	checkForUpdates();
     }
 
-    @Scheduled(cron = "0 0 1 * * MON-SAT", zone = "America/Sao_Paulo")
+    @Scheduled(cron = "0 0 1 * * MON-SAT", zone = zone)
     public void checkForNewResults01() {
-	System.out.println("The time is now " + Instant.now());
 	checkForUpdates();
     }
 
     public void checkForUpdates() {
+	System.out.println("checkForUpdates " + getCurrentDate());
+	
 	try {
 	    loteriasUpdate.checkForUpdates();
 	} catch (Exception e) {
